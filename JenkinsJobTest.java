70237005687b9709f159a3af8721a4435d72b8b0
unknown <sachin.phavade@gmail.com>
2018-02-18 16:21:34 +0530

    Updated code with 10 iteration

--- JenkinsJobTest.java
+++ JenkinsJobTest.java
@@ -1,6 +1,6 @@
 class JenkinsJobTest {
 	public static void main(String args[]) {
-		for(int i=1;i<=15;i++) {
+		for(int i=1;i<=10;i++) {
 			System.out.println("Welcome to Jenkins's World ... " + i);
 		}
 	}
